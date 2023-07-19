28250f3a7983e6a4ab23d683343a39a8ab700794
DarienneLancaster <101891701+DarienneLancaster@users.noreply.github.com>
2023-07-19 13:55:46 -0700

    Cleaned up import/filter/export function script

--- scripts/Filter_Selection_Tables.r
+++ scripts/Filter_Selection_Tables.r
@@ -1,33 +1,24 @@
-# some example code usring Rraven to bring in and export selection tables
-source("scripts/install_packages.R")#bring in load package script
+#script with function to import localization tables, filter localizations to 
+#within the camera FOV, and export into filtered data folder in wdata
+
+source("scripts/install_packages.R")#bring in install_packages script
 lp("Rraven")#use load package script to check for and load packages
 lp("tidyverse")
 lp("lubridate")
 
-install.packages("Rraven")
-library(Rraven)
-library(dplyr)
-install.packages("base")
-library(base)
-install.packages("rgl")
-library(rgl)
-
-#working code to import localization files, filter localizations within FOV of
-#cameras then export as raven table.  Currently only running one file at a time,
-#need to write loop
-
+#change folder name here for other study sites
 loc<-"Taylor_Islet_LA_2022"
 
-
+#start of import/filter/export function
 filt<-function(loc, 
-               err_span=2,
-               box_width=1
+               err_span=2,#specifies error span filter limit
+               box_width=1 #specifies distance either side of array center filter limit (e.g. 1m any direction. If you want higher limit for vertical(y) axis you can adjust within function (e.g. box_width*3 = 3m))
                ){
 
-data_files<- list.files(paste0("odata/", loc))
+data_files<- list.files(paste0("odata/", loc))# create object with list of all files in loc folder
 data_files
-out_dir<-paste0("wdata/",loc,"_filtered_",err_span,"_",box_width,"/")
-dir.create(out_dir, showWarnings = FALSE) #create new folder for filtered data
+out_dir<-paste0("wdata/",loc,"_filtered_",err_span,"_",box_width,"/") # creates out directory in working data(wdata) folder with
+dir.create(out_dir, showWarnings = FALSE) #create new folder for filtered data that specifies filter parameters (e.g. Taylor_Islet_LA_2022_filtered_2_1)
 
 for (i in 1:length(data_files)) {
   
@@ -37,13 +28,13 @@ yourobject<-imp_raven(path=paste0("odata/", loc),# the path to the FOLDER where 
                        all.data = TRUE)#if FALSE only a portion of the columns are imported
 
 #filter data
-testfilter<- yourobject %>%
+locfilter<- yourobject %>%
   # select(-Class, -Sound.type, -Software)%>% # can use this format to remove unnecessary columns if you want
   filter(x_err_span_m <err_span, y_err_span_m <err_span, z_err_span_m <err_span, 
          x_m <=box_width,x_m >=-box_width, y_m <=box_width,y_m >=-box_width,  z_m <=box_width,z_m >=-box_width*3)
 
 #write data
-write.table(testfilter,# the object you want to export as a selection table 
+write.table(locfilter,# the object you want to export as a selection table 
             file = paste0(out_dir, data_files[i]),# the path and file name using the file extension .txt
             sep = "\t", #how to delineate data
             row.names = FALSE, #row names will mess things up
@@ -51,53 +42,10 @@ write.table(testfilter,# the object you want to export as a selection table
 
 }}
 
-filt(loc="Taylor_Islet_LA_2022")
-
-#Code to plot coordinates, need to come up with a way to plot coordinates for 
-#localization one by one and save image with localization name.
-plot3d(x=testfilter$x_m, y=testfilter$y_m, z=testfilter$z_m)
+filt(loc)
 
 
-#trying to write loop for file processing
 
-extension <- "txt"
-fileNames <- Sys.glob(paste("*.", extension, sep = ""))
-fileNumbers <- seq(fileNames)
-for (fileNumber in fileNumbers) {
-  newFileName <-  paste("*filtered.", 
-                        sub(paste("\\.", extension, sep = ""), "", fileNames[fileNumber]), 
-                        ".", extension, sep = "")
-  # read old data:
-  sample <- read.csv(fileNames[fileNumber],
-                     header = TRUE,
-                     sep = ",")
-  # add one to every widget value in every file:
-  sample$Widgets <- sample$Widgets + 1
-  
-  # write old data to new files:
-  write.table(sample, 
-              newFileName,
-              append = FALSE,
-              quote = FALSE,
-              sep = ",",
-              row.names = FALSE,
-              col.names = TRUE)
-}
-fileNames <- Sys.glob("*.txt")
 
-for (fileName in fileNames) {
-  # read data:
-  
-  # add more stuff here
-}
-
-data_files<- list.files("C:/Users/dlanc/Documents/PhD/RavenPro/Localization_Results_Bamfield_2022/Taylor_Islet_LA_2022")
-data_files
-
-for(i in 1:length(data_files)) {                              # Head of for-loop
-  assign(paste0("data", i),                                   # Read and store data frames
-         read.table(paste0("C:/Users/dlanc/Documents/PhD/RavenPro/Localization_Results_Bamfield_2022/Taylor_Islet_LA_2022",
-                           data_files[i])))
-}
 
 
