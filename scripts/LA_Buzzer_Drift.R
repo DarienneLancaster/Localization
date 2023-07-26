6eaf2af86a743374e3303b77d62105c3b68d83a6
DarienneLancaster <101891701+DarienneLancaster@users.noreply.github.com>
2023-07-26 13:51:15 -0700

    -added milliseconds to buzzer drift times -subtract log time from amar buzzer time -create dataframe with video file names and pull out date/time stamp from file names -To Do list for next steps

--- scripts/LA_Buzzer_Drift.R
+++ scripts/LA_Buzzer_Drift.R
@@ -6,34 +6,53 @@ lp("readr")
 lp("hms")
 
 #load buzzer drift csv
-buzzerd<-read_csv("odata/LA_Buzzer_Drift_20230725 .csv")
+buzzerd<-read.csv("odata/Large Array buzzer times 20230726.csv", stringsAsFactors = FALSE)
 head(buzzerd)
-
-#remove extra columns from buzzerd
-buzzerdrift <- buzzerd %>% 
-  select(-13:-42)
-
-str(buzzerdrift)
-
-#add milliseconds to times
+glimpse(buzzerd)
+str(buzzerd)
+#colnames(buzzerd)[1]<-"Deployment_ID" #rename first column, this is necessary sometimes if a spreadsheet has been opened on a Mac and there's a weird column name
 
 
-#subtract buzzer log time (L) from AMAR buzzer time (A)
+#remove extra columns from buzzerd (this is useful code if you end up having extra columns in your imported dataframe)
+#buzzerdrift <- buzzerd %>% 
+# select(-13:-42) %>%
+#filter(!is.na(Deployment_ID))
 
-buzzerdrift$FC1drift <- (buzzerdrift$FC1A - buzzerdrift$FC1L)
-buzzerdrift$FC1drift<- as_hms(buzzerdrift$FC1drift)
+#convert FishCam buzzer time columns to time format and subtract log times from AMAR times to calculate drift
+buzzerd2<-buzzerd%>%
+  mutate(FC1L=as.POSIXct(paste(Date,FC1L), format="%Y-%m-%d %H:%M:%OS"),
+         FC1A=as.POSIXct(paste(Date,FC1A), format="%Y-%m-%d %H:%M:%OS"),
+         FC1drift=as_hms(FC1A - FC1L),
+         
+         FC2L=as.POSIXct(paste(Date,FC2L), format="%Y-%m-%d %H:%M:%OS"),
+         FC2A=as.POSIXct(paste(Date,FC2A), format="%Y-%m-%d %H:%M:%OS"),
+         FC2drift=as_hms(FC2A - FC2L),
+         
+         FC3L=as.POSIXct(paste(Date,FC3L), format="%Y-%m-%d %H:%M:%OS"),
+         FC3A=as.POSIXct(paste(Date,FC3A), format="%Y-%m-%d %H:%M:%OS"),
+         FC3drift=as_hms(FC3A - FC3L))
 
-buzzerdrift$FC2drift <- (buzzerdrift$FC2A - buzzerdrift$FC2L)
-buzzerdrift$FC2drift<- as_hms(buzzerdrift$FC2drift)
+#Creating column with video file names and pulling out dates from filenames (e.g. 2557_FishCam03_20220823T204721.521716Z_1600x1200_awb-auto_exp-night_fr-10_q-20_sh-0_b-50_c-0_i-400_sat-0)
 
-buzzerdrift$FC3drift <- (buzzerdrift$FC3A - buzzerdrift$FC3L)
-buzzerdrift$FC3drift<- as_hms(buzzerdrift$FC3drift)
+# create dataframe with list of all video files in folder
+data_files<- data.frame(vidnames=list.files("odata/Taylor_Islet_LA_2022_FC3_Videos"))
 
-#Add column with video file names (e.g. 2557_FishCam03_20220823T204721.521716Z_1600x1200_awb-auto_exp-night_fr-10_q-20_sh-0_b-50_c-0_i-400_sat-0)
+#pull time stamp out of video file names
+data_files2<- data_files %>%
+  #separate file number from file name and put in column "filenum" (e.g. 2557) and remove _FishCam text completely 
+  separate(vidnames, into = c("filenum","fileend"), sep = "_FishCam", remove = FALSE)%>% 
+  #chop up fileend into camera number (e.g. 03), underscore, and then pull apart Year, Month,Day, Hour, Min, Sec, Millisec (sep = pulls apart at # from start of fileend string)
+  separate(fileend, into = c("camnum", "ext", "y", "m", "d", "T","hr", "mn", "sc", "remaining"), sep = c(2,3,7,9,11,12,14,16,24) )%>%
+  #remove unnecessary columns (underscore, T between Date/Time, rest of file name)
+  select(-ext,-T,-remaining)%>%
+  #format date/time columns into same format as buzzerdrift times
+  mutate(vidstarttime = as.POSIXct(paste(y, m, d,hr, mn, sc), format="%Y %m %d %H %M %OS"))
 
-#Create column with time from video names (e.g. 20:47:21)
-#possible problem with 3 digit vs 4 digit start numbers
+second(data_files2$vidstarttime)#shows whether milliseconds are actually part of the time stamp data
 
-#Add drift times to video times (e.g. 20:47:21 + FC3drift)
+#TO DO
+#Create dataframe with localization info + buzzerdrift times + video file times and names
+#Create new column with drift times added to video times (e.g. 20:47:21 + FC3drift)
+#do interval calculation to predict which file each localization will occur in (factoring in buzzer drift times)
 
 
