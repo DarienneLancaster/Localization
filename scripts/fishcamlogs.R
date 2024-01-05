d5eda304893f7d2952a2de58ae73fd5b75d487f2
DarienneLancaster <101891701+DarienneLancaster@users.noreply.github.com>
2024-01-04 16:30:02 -0800

    calculate adjusted localization times using buzzer drift times and subtracting from fish sound localization times

--- scripts/fishcamlogs.R
+++ scripts/fishcamlogs.R
@@ -6,9 +6,9 @@ lp("lubridate")
 lp("readr")
 lp("hms")
 lp("readbulk")
+lp("readxl")
 
-######
-#import buzzer times from logs
+#######import buzzer times from logs######
 
 fold<-"FC3logs"
 
@@ -43,5 +43,80 @@ logtimes<-logtimes%>%
 
 #export to wdata
 write.csv(logtimes, "wdata/logtimes.csv", row.names = FALSE)
+
+#######Calculating Mean Drift per day per camera#####
+
+#load in updated logtimes file with AMAR buzzer times added and out of water
+#times removed
+buzzdrift<-read.csv("odata/logtimes_20240104_final.csv", header = TRUE)
+
+#remove additional columns from .csv
+buzzdrift<-buzzdrift%>%
+  select(-c(8:14))%>%
+  drop_na(Drift)
+#convert Drift to time
+buzzdrift$Drift<-as.POSIXct(buzzdrift$Drift, format= "%H:%M:%OS")
+
+str(buzzdrift)
+
+#Calculate mean and sd of drift by day for each cam and site
+driftmean<- buzzdrift%>%
+  group_by(Site, Cam, Date)%>%
+  summarise(meanDrift= mean(Drift), sdDrift= sd(Drift))
+
+#remove date from meanDrift  column
+driftmean2<- driftmean %>%
+  separate(meanDrift, into = c("WrongDate","meanDrift"), sep = " ", remove = TRUE)%>%
+  select(-c(4)) #remove WrongDate column
+
+write.csv(driftmean2,"wdata/driftmean.csv", row.names = FALSE)
+
+#NOTE - manually added in missing dates for fishcam 2 Ohiat Island  using
+#mean of day before and after (saved in odata as driftmean_missingFC2added.csv)
+#mean per day inspected for large/supsicious variations (one change made, now all looks good.)
+
+######add buzzerdrift times to localization files (use files that have been annotated for Fish Sounds)#####
+
+#import localization files annotated for Fish Sounds
+filtlocs<-"TestFSLocs" #change folder name here
+
+FSlocs<-imp_raven(path = paste0("wdata/", filtlocs), all.data =  TRUE)
+FSlocs<-FSlocs%>%
+  select(-c(37))%>% #a weird additional column was added at the end so needed to remove (may not always need this
+  filter(f=="FS")%>%#filter to only keep files labelled as fish sound (FS)
+  rename("Time" = "Begin Clock Time")
+
+str(FSlocs)
+
+#import edited driftmean csv
+driftedit<-read.csv("odata/driftmean_missingFC2added.csv", header = TRUE)
+str(driftedit)
+
+#change date column to match format of localization raven tables.
+driftedit2<- driftedit%>%
+  mutate(across("Date", str_replace, "2022-08-", "2022/8/"))%>%
+  mutate(across("Date", str_replace, "2022-09-", "2022/8/"))%>%
+  rename("Begin Date" = "Date")
   
+
+
+str(driftedit2)
+
+#join fish sound localization files to buzzerdrift times (this will throw a many to many error, this is good and because of
+#the different values for each of the 3 cameras.  There should be a line for each localization tied to each camera)
+locswdrift<-left_join(FSlocs, driftedit2, by = "Begin Date")
+
+#convert meanDrift and Time columns to time
+locswdrift$meanDrift<- as.POSIXct(locswdrift$meanDrift, format= "%H:%M:%OS")
+locswdrift$Time<- as.POSIXct(locswdrift$Time, format= "%H:%M:%OS")
+#subtract drift time from localization to get adjusted time of fish calls.
+locswdrift$locadjust<-as_hms(locswdrift$Time - locswdrift$meanDrift)
+
+  
+
+
+
+
+
+
 
