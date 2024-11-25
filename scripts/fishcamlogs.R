081b9dd9f02a98ddbb217ffdc10ec0207978e1d2
DarienneLancaster <101891701+DarienneLancaster@users.noreply.github.com>
2024-11-25 12:05:05 -0800

    Added code to check difference between eventmeasure and imageJ length measurements

--- scripts/fishcamlogs.R
+++ scripts/fishcamlogs.R
@@ -418,7 +418,7 @@ SoundnVid["t"][SoundnVid["t"] == 'k'] <- "d" #change any k annotations to d for 
 SoundnVid$t<-ifelse(SoundnVid$s=="e" & SoundnVid$t=="check", "e", SoundnVid$t) 
 
 
-write.csv(SoundnVid,"wdata/SoundnVid_20241108.csv", row.names = FALSE)
+write.csv(SoundnVid,"wdata/SoundnVid_20241114.csv", row.names = FALSE)
 #####
 ####create new localization plots for fish measurements in EventMeasure####
 
@@ -460,19 +460,101 @@ for( i in 1:nrow(FishM)){
 
 ##########match fish measurements to existing soundnvid data##########
 
-fish_length<-read.csv("odata/fish_measurements_20241108.csv", header = TRUE, skip = 4 )
+fish_length<-read.csv("odata/fish_measurements_20241125.csv", header = TRUE, skip = 4 )
 
+fish_lengthIJ<-read.csv("odata/TI_ImageJ_screenshots/TI_fishmeasurements_nonstereo.csv", header = TRUE)
+
+#############################################################################
+#check relationship between EventMeasure and ImageJ fish length measurements
+
+#add zeros to start of Localization ID
+fish_lengthIJ$fishID<-with_options(
+  c(scipen = 999), 
+  str_pad(fish_lengthIJ$fishID, 4, pad = "0")
+)
+
+#create fishID column and average 2 length measurements
+fish_lengthIJD<-fish_lengthIJ%>%
+  filter(grepl("D", Notes))%>%  #remove any rows with D in notes column (D for Duplicate measurement)
+  unite(fishID, fishID, Date, sep = "_", remove = FALSE)%>%
+  group_by(fishID) %>%
+  summarize(mean_lengthIJ = mean(Length, na.rm = TRUE),
+            sd_lengthIJ = sd(Length, na.rm = TRUE)) %>%
+  ungroup()
+
+#add zeros to start of Localization ID
+fish_length$Selection<-with_options(
+  c(scipen = 999), 
+  str_pad(fish_length$Selection, 4, pad = "0")
+)
+
+###do the same for EventMeasure data###
+fish_lengthEMD<-fish_length%>%
+  dplyr::select(-c(3:5,7:28, 33))%>% #get rid of unnecessary columns
+  filter(grepl("D", Notes))%>%  #remove any rows with D in notes column (D for Duplicate measurement)
+  separate(Filename, into = c("vidnum","CamName", "Date"), sep = "_", remove = FALSE)%>%
+  separate(Date, into = c("Date"), sep = "T")%>%
+  rename(Length =Length..mm.)%>%
+  unite(fishID, Selection, Date, sep = "_", remove = FALSE)%>%
+  group_by(fishID) %>%
+  summarize(mean_lengthEM = mean(Length, na.rm = TRUE),
+            sd_lengthEM = sd(Length, na.rm = TRUE)) %>%
+  ungroup()
+
+#join EM and IJ duplicate measure dataframes
+dup_meas<-left_join(fish_lengthEMD, fish_lengthIJD, by = "fishID")
+
+#subract EM lengths from IJ lengths and calculate mean and SD difference
+dup_meas$difference<- dup_meas$mean_lengthEM-dup_meas$mean_lengthIJ
+
+mean(dup_meas$difference)
+sd(dup_meas$difference)
+#EventMeasure lengths are an average of 17mm longer than ImageJ measurements with a standard deviation of 48mm. 
+#Strong siginificant positive correlation between measurements (rho =0.7, p=0.02)
+
+#Pearsons correlation between measurements
+measure_cor<-cor.test(dup_meas$mean_lengthEM, dup_meas$mean_lengthIJ,  method="pearson") #exact = FALSE gets rid of impact of tied values on ranking
+print(measure_cor)
+#rho = 0.52, p = 0.102 (weak significant positive relationship)
+#interpret results - looking for rho value between - 1 and +1 with significant p value
+# .00-.19 “very weak”
+#  .20-.39 “weak”
+#  .40-.59 “moderate”
+#  .60-.79 “strong”
+#  .80-1.0 “very strong”
+
+lp("smplot2")  #package that allows you to add correlation stats to graphs
+
+# Create ggplot plots of rugosity metrics
+measurements <- ggplot(dup_meas, aes(x = mean_lengthEM, y = mean_lengthIJ)) +
+  geom_point(color = "deepskyblue") +
+  labs(title = "a)") +
+  sm_statCorr(corr_method="pearsons", color = "deepskyblue3", linetype = "dashed")+
+  labs(x = "EM", y = "IJ")+
+  theme_bw()+
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
+measurements 
+
+#############################################################################################
+###create new dataframe with fish measurements joined to sound data
+
+#create fishID column and average 2 length measurements
+fish_lengthIJ1<-fish_lengthIJ%>%
+  filter(!grepl("D", Notes))%>%  #remove any rows with D in notes column (D for Duplicate measurement)
+  unite(fishID, fishID, Date, sep = "_", remove = FALSE)%>%
+  group_by(fishID) %>%
+  summarize(mean_length = mean(Length, na.rm = TRUE),
+            sd_length = sd(Length, na.rm = TRUE)) %>%
+  ungroup()
+
+duplicated(fish_lengthIJ1$fishID)
+
+###do the same for EventMeasure data###
 fish_length1<-fish_length%>%
   dplyr::select(-c(3:5,7:28, 33:34))%>% #get rid of unnecessary columns
   separate(Filename, into = c("vidnum","CamName", "Date"), sep = "_", remove = FALSE)%>%
   separate(Date, into = c("Date"), sep = "T")%>%
   rename(Length =Length..mm.)
-
-#add zeros to start of Localization ID
-fish_length1$Selection<-with_options(
-  c(scipen = 999), 
-  str_pad(fish_length1$Selection, 4, pad = "0")
-)
 
 #combine fishnum (Selection) and Date
 fish_length1<-fish_length1%>%
@@ -482,7 +564,32 @@ fish_length1<-fish_length1%>%
             sd_length = sd(Length, na.rm = TRUE)) %>%
   ungroup()
 
-SoundnVid1<-left_join(SoundnVid, fish_length1, by = "fishID")
+duplicated(fish_length1$fishID)
+
+#join EM and ImageJ measurements
+measures<- rbind(fish_length1, fish_lengthIJ1)
+duplicated(measures$fishID)
+
+#link measures to sound dataframe
+SoundnVid1<-left_join(SoundnVid, measures, by = "fishID")
+
+### look at fish length histogram by species
+
+FishLength<-SoundnVid1%>%
+  group_by(fishID) %>%
+  slice_head(n = 1) %>%
+  filter(!is.na(mean_length))%>% ## get rid of NAs in length (these fish haven't been annotated yet)
+  ungroup()
+
+
+#histogram of fish lengths by species (bins of 5)
+ggplot(FishLength, aes(x = mean_length)) +
+  geom_histogram(binwidth = 5, color = "black", fill = "darkturquoise") +
+  facet_wrap(~ Species, scales = "free_y") +
+  labs(title = "Histograms of Mean Length by Species", x = "Mean Length", y = "Count") +
+  coord_cartesian(ylim = c(0, 8)) +  # Fix y-axis range from 0 to 8
+  theme_classic()
+
 
 
 # ####create test plots####
